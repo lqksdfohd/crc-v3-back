265fd258c62475a60fc12be227aa47c2ab43fd5d
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-01-08 20:17:14 +0100

    feat(ProjetControlleur): créer la méthode créer un projet.

--- src/main/java/com/app/crc/controlleurs/ProjetControlleur.java
+++ src/main/java/com/app/crc/controlleurs/ProjetControlleur.java
@@ -2,22 +2,29 @@ package com.app.crc.controlleurs;
 
 
 import com.app.crc.dtos.ProjetDto;
+import com.app.crc.entites.Projet;
+import com.app.crc.services.MapstructService;
+import com.app.crc.services.ProjetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProjetControlleur {
 
+    @Autowired
+    private ProjetService projetService;
+
+    @Autowired
+    private MapstructService mapstructService;
+
     @PostMapping(value = "/projet",consumes = MediaType.APPLICATION_JSON_VALUE,
     produces = MediaType.APPLICATION_JSON_VALUE)
     public ProjetDto creerProjet(@RequestBody ProjetDto dto){
-        ProjetDto output = new ProjetDto();
-        output.setNomProjet(dto.getNomProjet());
-        output.setId(101);
-        return output;
+        Projet input = mapstructService.projetDtoVersProjet(dto);
+        Projet resultat = projetService.creerProjet(input);
+        return mapstructService.projetVersProjetDto(resultat);
     }
 }
