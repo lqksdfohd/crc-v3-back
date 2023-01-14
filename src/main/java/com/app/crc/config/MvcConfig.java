42981560063a655ed3fda01953ceead27cf257e6
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-01-14 19:09:25 +0100

    build(MvcConfig):permettre l'acces au back au serveur angular

---
+++ src/main/java/com/app/crc/config/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.app.crc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedMethods(new String[]{"POST", "GET","DELETE"})
+                .allowedOrigins(new String[]{"http://localhost:4200"});
+    }
+}
