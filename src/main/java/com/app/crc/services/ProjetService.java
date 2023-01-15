600f4e393592bace7e9f9ca8b3a84b2c0737f29a
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-01-15 15:30:36 +0100

    feature(ProjetService) creer la méthode récuperer tous les projets en base

--- src/main/java/com/app/crc/services/ProjetService.java
+++ src/main/java/com/app/crc/services/ProjetService.java
@@ -5,7 +5,10 @@ import com.app.crc.repository.ProjetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Service
 public class ProjetService {
@@ -20,5 +23,12 @@ public class ProjetService {
         }else{
             return projetRepository.save(projet);
         }
+    }
+
+    public List<Projet> recupererTousLesProjets(){
+        Iterable<Projet> iterable = projetRepository.findAll();
+        List<Projet> output = new ArrayList<>();
+        iterable.forEach(p -> output.add(p));
+        return output;
     }
 }
