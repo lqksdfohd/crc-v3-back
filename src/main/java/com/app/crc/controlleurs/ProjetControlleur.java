7980af4d9a450636cc94cdd36b6ace30b8817034
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-01-15 15:32:21 +0100

    feature(ProjetControlleur): lister les projets existants

--- src/main/java/com/app/crc/controlleurs/ProjetControlleur.java
+++ src/main/java/com/app/crc/controlleurs/ProjetControlleur.java
@@ -8,12 +8,14 @@ import com.app.crc.services.ProjetService;
 import com.app.crc.services.ProjetValidatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -40,5 +42,13 @@ public class ProjetControlleur {
         Projet input = mapstructService.projetDtoVersProjet(dto);
         Projet resultat = projetService.creerProjet(input);
         return mapstructService.projetVersProjetDto(resultat);
+    }
+
+
+    @GetMapping(value = "/projets", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<ProjetDto> listerLesProjetExistants(){
+        List<Projet> fromBase = projetService.recupererTousLesProjets();
+        List<ProjetDto> output = mapstructService.listeProjetVersListeProjetDto(fromBase);
+        return output;
     }
 }
