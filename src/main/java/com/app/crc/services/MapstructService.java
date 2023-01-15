cf60586398bc126853b055c06cdaa76a70aac0c6
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-01-15 15:31:28 +0100

    feature(MapstructService) map de liste de projet vers liste de dtos

--- src/main/java/com/app/crc/services/MapstructService.java
+++ src/main/java/com/app/crc/services/MapstructService.java
@@ -6,10 +6,14 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface MapstructService {
     Projet projetDtoVersProjet(ProjetDto dto);
 
     ProjetDto projetVersProjetDto(Projet projet);
+
+    List<ProjetDto> listeProjetVersListeProjetDto(List<Projet> liste);
 }
