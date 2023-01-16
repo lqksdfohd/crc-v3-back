0d7a3ec4824cf12df1457fa9d7b567d3ba9ed26c
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-01-16 11:41:42 +0100

    feature(MapstructService): creer la méthode pour mapper de projet vers ProjetCompletDto

--- src/main/java/com/app/crc/services/MapstructService.java
+++ src/main/java/com/app/crc/services/MapstructService.java
@@ -1,5 +1,6 @@
 package com.app.crc.services;
 
+import com.app.crc.dtos.ProjetCompletDto;
 import com.app.crc.dtos.ProjetDto;
 import com.app.crc.entites.Projet;
 import org.mapstruct.Mapper;
@@ -14,6 +15,8 @@ public interface MapstructService {
     Projet projetDtoVersProjet(ProjetDto dto);
 
     ProjetDto projetVersProjetDto(Projet projet);
+
+    ProjetCompletDto projetVersProjetCompletDto(Projet projet);
 
     List<ProjetDto> listeProjetVersListeProjetDto(List<Projet> liste);
 }
