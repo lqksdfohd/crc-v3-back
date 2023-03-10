a9b2cd79ba5bbcd1978f4d334d9fa42478257e51
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-03-10 18:16:21 +0100

    build: mettre à jour le jeu de données de départ de l'application

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into projet (projet_id, nom) values (10, 'projet existant');
-insert into projet(projet_id, nom) values (90, 'projet test 90');
+insert into projet(projet_id, nom) values (90, 'todo-list');
 
-insert into klass(klass_id,nom,projet_id) VALUES (40, 'klass test 40', 90);
-insert into klass(klass_id, nom, projet_id) values (41, 'klass test 41', 90 );
-insert into responsabilite(responsabilite_id, titre, klass_id) values (50, 'responsabilite test 50', 40);
+insert into klass(klass_id,nom,projet_id) VALUES (40, 'todo', 90);
+insert into klass(klass_id, nom, projet_id) values (41, 'tache', 90 );
+insert into responsabilite(responsabilite_id, titre, klass_id) values (50, 'afficher une liste de tâches actionnables', 40);
+insert into responsabilite(responsabilite_id, titre, klass_id) values(51, 'décrire une étape d un objectif',41);
 insert into collaborateur(collaborateur_id, principal_id, collaborant_id) values (60, 40,41);
